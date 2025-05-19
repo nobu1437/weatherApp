7624ef0f86a2d931f5c56f24952c2fa24c089686
nobu1437 <omabrirdu@yandex.ru>
2025-05-19 19:33:25 +0500

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Andrey Nobu on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by Andrey Nobu on 19.05.2025.
+//
+
+import Testing
+@testable import weatherApp
+
+struct weatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
